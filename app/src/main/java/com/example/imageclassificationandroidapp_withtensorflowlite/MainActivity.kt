c3e92c56262571c929178c6ed0e4276beb90e94d
mohamedtamer0 <mt4452242@gmail.com>
2022-02-06 04:09:58 +0200

    update

--- app/src/main/java/com/example/imageclassificationandroidapp_withtensorflowlite/MainActivity.kt
+++ app/src/main/java/com/example/imageclassificationandroidapp_withtensorflowlite/MainActivity.kt
@@ -1,11 +1,102 @@
 package com.example.imageclassificationandroidapp_withtensorflowlite
 
+import android.Manifest
+import android.content.Intent
+import android.content.pm.PackageManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.provider.MediaStore
+import android.util.Log
+import android.widget.Button
+import android.widget.ImageView
+import android.widget.TextView
+import android.widget.Toast
+import androidx.activity.result.ActivityResult
+import androidx.activity.result.contract.ActivityResultContracts
+import androidx.core.content.ContextCompat
+import com.example.imageclassificationandroidapp_withtensorflowlite.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var imageView: ImageView
+    private lateinit var button: Button
+    private lateinit var tvOutput: TextView
+    private val GALLERY_REQUEST_CODE = 123
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        imageView = binding.imageView
+        button = binding.btnCaptureImage
+        tvOutput = binding.tvOutput
+        val buttonLoad = binding.btnLoadImage
+
+        button.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    android.Manifest.permission.CAMERA
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                takePicturePreview.launch(null)
+            } else {
+                requestPermission.launch(Manifest.permission.CAMERA)
+            }
+        }
+
+        buttonLoad.setOnClickListener {
+            if (ContextCompat.checkSelfPermission(
+                    this,
+                    Manifest.permission.READ_EXTERNAL_STORAGE
+                ) == PackageManager.PERMISSION_GRANTED
+            ) {
+                val intent =
+                    Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI)
+                intent.type = "image/*"
+                val mimeTypes = arrayOf("image/jpeg", "image/png", "image/jpg")
+                intent.putExtra(Intent.EXTRA_MIME_TYPES, mimeTypes)
+                intent.flags = Intent.FLAG_GRANT_READ_URI_PERMISSION
+                onresult.launch(intent)
+            } else {
+                requestPermission.launch(Manifest.permission.READ_EXTERNAL_STORAGE)
+            }
+        }
+
     }
+
+
+    //request camera permission
+    private val requestPermission =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { granted ->
+            if (granted) {
+                takePicturePreview.launch(null)
+            } else {
+                Toast.makeText(this, "Permission Denied !! Try again", Toast.LENGTH_SHORT).show()
+            }
+        }
+
+
+    //launch camera and take picture
+    private val takePicturePreview =
+        registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
+            if (bitmap != null) {
+                imageView.setImageBitmap(bitmap)
+
+            }
+        }
+
+    //to get image from gallery
+    private val onresult =
+        registerForActivityResult(ActivityResultContracts.StartActivityForResult()) { result ->
+            Log.i("TAG", "This is the result: ${result.data} ${result.resultCode}")
+            onResultReceived(GALLERY_REQUEST_CODE, result)
+        }
+
+    private fun onResultReceived(requestCode: Int, result: ActivityResult?) {
+
+    }
+
+
 }
