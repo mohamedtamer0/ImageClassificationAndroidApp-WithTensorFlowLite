9f68f8548c2cad1d9f8801a3f33dfd450644399c
mohamedtamer0 <mt4452242@gmail.com>
2022-02-06 05:19:50 +0200

    update

--- app/src/main/java/com/example/imageclassificationandroidapp_withtensorflowlite/MainActivity.kt
+++ app/src/main/java/com/example/imageclassificationandroidapp_withtensorflowlite/MainActivity.kt
@@ -1,8 +1,15 @@
 package com.example.imageclassificationandroidapp_withtensorflowlite
 
 import android.Manifest
+import android.app.Activity
+import android.app.AlertDialog
+import android.content.ContentValues
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
+import android.graphics.BitmapFactory
+import android.graphics.drawable.BitmapDrawable
+import android.net.Uri
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.provider.MediaStore
@@ -15,6 +22,9 @@ import androidx.activity.result.ActivityResult
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.core.content.ContextCompat
 import com.example.imageclassificationandroidapp_withtensorflowlite.databinding.ActivityMainBinding
+import com.example.imageclassificationandroidapp_withtensorflowlite.ml.BridsModel
+import org.tensorflow.lite.support.image.TensorImage
+import java.io.IOException
 
 class MainActivity : AppCompatActivity() {
 
@@ -64,6 +74,24 @@ class MainActivity : AppCompatActivity() {
             }
         }
 
+
+        //to redirect user to google search for the scientific name
+        tvOutput.setOnClickListener {
+            val intent = Intent(
+                Intent.ACTION_VIEW,
+                Uri.parse("https://www.google.com/search?q=${tvOutput.text}")
+            )
+            startActivity(intent)
+        }
+
+
+        // to download image when longPress on ImageView
+        imageView.setOnLongClickListener {
+            requestPermissionLauncher.launch(Manifest.permission.WRITE_EXTERNAL_STORAGE)
+            return@setOnLongClickListener true
+        }
+
+
     }
 
 
@@ -83,7 +111,7 @@ class MainActivity : AppCompatActivity() {
         registerForActivityResult(ActivityResultContracts.TakePicturePreview()) { bitmap ->
             if (bitmap != null) {
                 imageView.setImageBitmap(bitmap)
-
+                outputGenerator(bitmap)
             }
         }
 
@@ -95,8 +123,98 @@ class MainActivity : AppCompatActivity() {
         }
 
     private fun onResultReceived(requestCode: Int, result: ActivityResult?) {
-
+        when (requestCode) {
+            GALLERY_REQUEST_CODE -> {
+                if (result?.resultCode == Activity.RESULT_OK) {
+                    result.data?.data?.let { uri ->
+                        Log.i("TAG", "onResultReceived: $uri")
+                        val bitmap =
+                            BitmapFactory.decodeStream(contentResolver.openInputStream(uri))
+                        imageView.setImageBitmap(bitmap)
+                        outputGenerator(bitmap)
+                    }
+                } else {
+                    Log.e("TAG", "onActivityResult: error in selecting image")
+                }
+            }
+        }
     }
 
+
+    private fun outputGenerator(bitmap: Bitmap) {
+//declaring tensor flow lite model variable
+        val birdModel = BridsModel.newInstance(this)
+
+        // converting bitmap into tensor flow image
+        val newBitmap = bitmap.copy(Bitmap.Config.ARGB_8888, true)
+        val tfimage = TensorImage.fromBitmap(newBitmap)
+
+        //process the image using trained model and sort it in descending order
+        val outputs = birdModel.process(tfimage)
+            .probabilityAsCategoryList.apply {
+                sortByDescending { it.score }
+            }
+        //getting result having high Probability
+        val highProbabilityOutput = outputs[0]
+
+        //setting output text
+        tvOutput.text = highProbabilityOutput.label
+        Log.i("TAG", "outputGenerator: $highProbabilityOutput")
+    }
+
+
+    // to download image to device
+    private val requestPermissionLauncher =
+        registerForActivityResult(ActivityResultContracts.RequestPermission()) { isGranted: Boolean ->
+            if (isGranted) {
+                AlertDialog.Builder(this).setTitle("Download Image?")
+                    .setMessage("Do You want to download this image to your device?")
+                    .setPositiveButton("Yes") { _, _ ->
+                        val drawable: BitmapDrawable = imageView.drawable as BitmapDrawable
+                        val bitmap = drawable.bitmap
+                        downloadImage(bitmap)
+                    }
+                    .setNegativeButton("No") { dialog, _ ->
+                        dialog.dismiss()
+                    }
+                    .show()
+            } else {
+                Toast.makeText(
+                    this,
+                    "Please allow permission to download image",
+                    Toast.LENGTH_SHORT
+                ).show()
+            }
+        }
+
+
+    // fun that takes a bitmap and store to user's device
+    private fun downloadImage(mBitmap: Bitmap): Uri? {
+        val contentValues = ContentValues().apply {
+            put(
+                MediaStore.Images.Media.DISPLAY_NAME,
+                "Birds_Images" + System.currentTimeMillis() / 1000
+            )
+            put(MediaStore.Images.Media.MIME_TYPE, "image/png")
+        }
+        val uri = MediaStore.Images.Media.EXTERNAL_CONTENT_URI
+        if (uri != null) {
+            contentResolver.insert(uri, contentValues)?.also {
+                contentResolver.openOutputStream(it).use { outputStream ->
+                    if (!mBitmap.compress(Bitmap.CompressFormat.PNG, 100, outputStream)) {
+                        throw IOException("Couldn't save the bitmap")
+                    } else {
+                        Toast.makeText(
+                            applicationContext,
+                            "Image Saved",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+                return it
+            }
+        }
+        return null
+    }
 
 }
